b7f31d014420b9f95725beb533f9896f45fb311f
rory quarrier <roryquarrier@gmail.com>
2022-06-06 10:06:43 -0300

    3-create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+      <main>list prodducts</main>
+    </div>
+  );
+}
+
+export default App;
